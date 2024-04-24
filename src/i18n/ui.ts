112dc4d04055de3493ede5c3b3675824e5be19b1
Marcin S. <marcin@realemail.net>
2024-04-24 22:08:25 +0200

    Start work on Rooms page; random fixes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -31,12 +31,20 @@ export const ui = {
         'nav.contact': 'Contact Us / FAQ',
         'nav.book': 'Book Now',
 
-        "index.title": "B&B Baia di Trentova",
+        "index.title": "Welcome",
         "index.description": "B&B Baia di Trentova is a family-run bed and breakfast located in Agropoli in Cilento. The B&B is located in a quiet area, just a few steps from the sea and the Trentova Bay.",
         "index.welcome": "Welcome to Cilento",
         "index.terrace": "Terrace with Bedroom",
         "index.seaview": "Seaview with Breakfast",
         "index.smile": "A Smile with Hosts",
+
+        "rooms.title": "Our Rooms",
+        "rooms.welcome.header": "Buongiorno",
+        "rooms.welcome": "Clean and comfortable accommodation. Enjoy the quiet and relaxed atmosphere, read a book or taste a good glass of wine.",
+        "rooms.description.header": "Our Rooms",
+        "rooms.description": "All five of our rooms are simple, clean and cozy. They are equipped with: an en-suite bathroom, minibar, hair-dryer, TV, Wi-Fi, courtesy set, and, most importantly, a small terrace outside to enjoy the beautiful view over the sea.",
+
+        "contact.title": "Contact Us",
     },
     it: {
         'site.name': 'B&B Baia di Trentova',
@@ -51,5 +59,13 @@ export const ui = {
         "index.terrace": "Terrazza con Camera",
         "index.seaview": "Vista Mare con Colazione",
         "index.smile": "Un Sorriso con gli Ospiti",
+
+        "rooms.title": "Le nostre Camere",
+        "rooms.welcome.header": "Buongiorno",
+        "rooms.welcome": "Alloggi puliti e confortevoli. Goditi l'atmosfera tranquilla e rilassata, leggi un libro o assapora un buon bicchiere di vino.",
+        "rooms.description.header": "Le nostre Camere",
+        "rooms.description": "Tutte e cinque le nostre camere sono semplici, pulite e accoglienti. Sono dotate di: bagno privato, minibar, asciugacapelli, TV, Wi-Fi, set di cortesia e, soprattutto, un piccolo terrazzo esterno per godere della bellissima vista sul mare.",
+
+        "contact.title": "Contattaci",
     },
 } as const;
