1430d62f351a240323d5ea9c8d2adf27a4470013
Marcin S. <marcin@realemail.net>
2024-04-28 14:02:36 +0200

    Add Reviews page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -16,6 +16,10 @@ export const paths = {
         it: 'camere',
         en: 'en/rooms',
     },
+    reviews: {
+        it: 'recensioni',
+        en: 'en/reviews',
+    },
     contact: {
         it: 'contattaci',
         en: 'en/contact',
@@ -29,7 +33,8 @@ export const ui = {
         
         'nav.rooms': 'Our Rooms',
         'nav.contact': 'Contact Us / FAQ',
-        'nav.book': 'Book Now',
+        'nav.reviews': 'Reviews',
+        'nav.book': 'Book Now!',
 
         "index.title": "Welcome",
         "index.description": "B&B Baia di Trentova is a family-run bed and breakfast located in Agropoli in Cilento. The B&B is located in a quiet area, just a few steps from the sea and the Trentova Bay.",
@@ -44,6 +49,9 @@ export const ui = {
         "rooms.description.header": "Our Rooms",
         "rooms.description": "All five of our rooms are simple, clean and cozy. They are equipped with: an en-suite bathroom, minibar, hair-dryer, TV, Wi-Fi, courtesy set, and, most importantly, a small terrace outside to enjoy the beautiful view over the sea.",
 
+        "reviews.title": "Reviews",
+        "reviews.description": "Read what our guests have to say about their stay with us!",
+
         "contact.title": "Contact Us",
         "contact.description": "We’re always happy to hear from you, so talk to us any time you like.<br><br>If your inquiry is urgent, please use one of the social media contacts listed below, otherwise, we will reply by email as soon as possible. Ciao!",
     },
@@ -52,7 +60,8 @@ export const ui = {
 
         'nav.rooms': 'Camere',
         'nav.contact': 'Contattaci / FAQ',
-        'nav.book': 'Prenota Ora',
+        'nav.reviews': 'Recensioni',
+        'nav.book': 'Prenota Ora!',
 
         "index.title": "B&B Baia di Trentova",
         "index.description": "Il B&B Baia di Trentova è un bed and breakfast a conduzione familiare situato ad Agropoli nel Cilento. Il B&B si trova in una zona tranquilla, a pochi passi dal mare e dalla Baia di Trentova.",
@@ -66,6 +75,9 @@ export const ui = {
         "rooms.welcome": "Alloggi puliti e confortevoli. Goditi l'atmosfera tranquilla e rilassata, leggi un libro o assapora un buon bicchiere di vino.",
         "rooms.description.header": "Le nostre Camere",
         "rooms.description": "Tutte e cinque le nostre camere sono semplici, pulite e accoglienti. Sono dotate di: bagno privato, minibar, asciugacapelli, TV, Wi-Fi, set di cortesia e, soprattutto, un piccolo terrazzo esterno per godere della bellissima vista sul mare.",
+
+        "reviews.title": "Recensioni",
+        "reviews.description": "Leggi cosa dicono i nostri ospiti del loro soggiorno con noi!",
 
         "contact.title": "Contattaci",
         "contact.description": "Siamo sempre felici di sentirti, quindi parla con noi in qualsiasi momento ti piaccia.<br><br>Se la tua richiesta è urgente, utilizza uno dei contatti sui social media elencati di seguito, altrimenti ti risponderemo via email il prima possibile. Ciao!",
